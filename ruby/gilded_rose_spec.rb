d674692d9a098f2cf347813c2c09a4653b0f9bb3
David Grieser <david.grieser@gmail.com>
2024-01-20 01:46:21 -0600

    baseline value increases

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -10,6 +10,45 @@ describe GildedRose do
     GildedRose.new(items).update_quality()
     expect(items[0].name).to eq "foo"
   end
+
+  context "Aged Brie" do
+    it "increase in quality the older it gets"
+  end
+
+  context "Backstage passes" do
+    let(:items) { [Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 1)] }
+    let(:gs) { GildedRose.new(items) }
+    subject { items[0].quality }
+
+    context "quality increases" do
+      it "by 1 when there are more than 10 day left" do
+        gs.update_quality()
+        expect(subject).to eq 2
+      end
+      it "by 2 when there are 10 days or less" do
+        gs.update_quality()
+        gs.update_quality()
+        gs.update_quality()
+        expect(subject).to eq 5
+      end
+      it "by 3 when there are 5 days or less" do
+        # 2 # 3
+        # 5 # 7 # 9 # 11 # 13
+        # 16
+        8.times { gs.update_quality }
+        expect(subject).to eq 16
+      end
+    end
+    it "quality is 0 when sell in is 0"
+  end
+  context "item is Aged Brie" do
+    context "and quality is less than 50" do
+      it "should update the quality by 1" do
+      end
+    end
+  end
+
+  context ""
 end
 
 describe Item do
