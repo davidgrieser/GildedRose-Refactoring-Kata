2c2ad9da9c682b0604985a4e322623ec82ab23f5
David Grieser <david.grieser@gmail.com>
2024-01-20 01:32:31 -0600

    add test for to_s for Item

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,4 +1,6 @@
 require 'rspec'
+require 'simplecov'
+SimpleCov.start
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
@@ -7,5 +9,12 @@ describe GildedRose do
     items = [Item.new("foo", 0, 0)]
     GildedRose.new(items).update_quality()
     expect(items[0].name).to eq "foo"
+  end
+end
+
+describe Item do
+  it "returns the name, sell in, and quality" do
+    item = Item.new("Leather Jacket", 10, 5)
+    expect(item.to_s).to eq "Leather Jacket, 10, 5"
   end
 end
