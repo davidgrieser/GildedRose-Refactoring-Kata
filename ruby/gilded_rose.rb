3769bc1d41d1c9f83aef8169b5a99324b1ae5fd4
David Grieser <david.grieser@gmail.com>
2024-01-21 00:31:15 -0600

    drop all older logic
    this now uses a class with custom logic as needed
    drops all the nested if statements

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -64,8 +64,6 @@ class GildedRose
         BackstagePass.new(item)
       elsif item.name == SPECIAL_ITEMS[2]
         Sulfuras.new(item)
-      elsif SPECIAL_ITEMS.include?(item.name)
-        item
       else
         BaseItem.new(item)
       end
@@ -74,53 +72,7 @@ class GildedRose
 
   def update_quality()
     @parsed_items.each do |item|
-      if item.class == BaseItem || item.class == AgedBrie || item.class == BackstagePass || item.class == Sulfuras
-        item.update
-        next
-      end
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item.update
     end
   end
 end
